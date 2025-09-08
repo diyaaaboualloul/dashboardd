474cc37152705b6fb4cdc5f45c97c0c96eee56ef
diyaaaboualloul <diyaaaboualloul@gmail.com>
2025-09-08 11:37:44 +0300

    removing cmt

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,4 +1,3 @@
-{{-- resources/views/components/layout.blade.php --}}
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,13 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>{{ $title ?? 'Dashboard - SB Admin' }}</title>
 
-    {{-- third-party CSS (optional) --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css">
-
-    {{-- YOUR css from /public/css --}}
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-
-    {{-- icons (optional) --}}
     <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
 </head>
 <body class="sb-nav-fixed">
@@ -28,13 +22,10 @@
             </main>
 
             <x-footer />
-        </div> {{-- /#layoutSidenav_content --}}
-    </div> {{-- /#layoutSidenav --}}
+        </div>
+    </div>
 
-    {{-- YOUR js from /public/js --}}
     <script src="{{ asset('js/app.js') }}"></script>
-
-    {{-- third-party JS (optional) --}}
-    <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2" ></script>
+    <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2"></script>
 </body>
 </html>
